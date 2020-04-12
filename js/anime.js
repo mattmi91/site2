556f3e5833414dd07a70a9b5a7fff05f826330f3
mattmi91 <63423576+mattmi91@users.noreply.github.com>
2020-04-12 15:52:42 -0400

    sss

---
+++ js/anime.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+$(document).ready(function(){
+  $("body").css("background", "#3a6a6e");
+  $(".ftco-bg-dark").css("background", "#326e42");
+  $(".am").css("background", "#1f1e1d");
+  $(".span1").css("color", "#3a6a6e");
+  $(".span2").css("color", "#8a2929");
+  $(".span3").css("color", "#7293e8");
+  $(".cc").css("background", "#8a2929");
+  $(".ps").css("background", "#7293e8");
+  $(".cm").css("background", "#2b1629");
+});
+
+
+
+$(document).ready(function(){
+  $(".hid").show();
+  $(".imgg11").hide();
+   $(".imgg22").hide();
+  $(".reappear1").hide();
+  $(".logo1").hide();
+	$(".logo").hide();
+	$(".dots1").hide();
+
+	
+});
+
+
+      
